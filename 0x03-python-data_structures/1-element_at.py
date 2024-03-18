a388bfd6a006972fa96f6b0384f867074caaedaf
ULydie <umuhirelydie2@gmail.com>
2024-03-18 06:20:30 -0700

    print element index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for list in my_list:
+        if idx < 0:
+            return none
+        if idx > len(my_list):
+            return none
+        else:
+            return my_list[idx]
