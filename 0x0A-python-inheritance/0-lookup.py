6bb5b677c8781507f50c0937fff1218f49454a29
ULydie <umulydie125@gmail.com>
2024-04-16 11:35:52 -0700

    fix indent error

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,5 +4,5 @@ contain codes of function
 """
 
 def lookup(obj):
-""" return list  of al attributes and method of object"""
+    """ return list  of al attributes and method of object"""
     return dir(obj)
