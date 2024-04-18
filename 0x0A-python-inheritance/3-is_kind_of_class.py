4a957a2c8ca3b667b964a53fb2109ef8b76aaaa0
ULydie <umulydie125@gmail.com>
2024-04-17 18:09:23 -0700

    inhertance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""
+contains function is_kind_of_class deatails
+"""
+
+def is_kind_of_class(obj, a_class):
+    """returns True if the object is an instance of class that inherited from"""
+    return (isinstance(obj, a_class))
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" class  geometry
+"""
+
+class BaseGeometry:
+
+    """empty class"""
+    pass
